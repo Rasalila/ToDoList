8c739d94f1159938e3b3da63336d1c0b2cc60d16
unknown <rasalila@list.ru>
2024-08-29 21:04:56 +0300

    is working! added scc style

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
 const taskInput = document.getElementById("taskInput");
-const addButton = document.getElementById("addBatton");
+const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 
-//Обработчик события клика на кнопку "Добавить"
-addButton.addEventListener("click", function() {
-  //Получаем значение, введенное в поле ввода
+addButton.addEventListener("click", function () {
   const taskText = taskInput.value;
-
-  //Создаём новый элемент списка
   const newTask = document.createElement("li");
-
-  //Устанавливаем текст задачи внутри созданного элемента списка
   newTask.textContent = taskText;
-
-  //Добавляем созданный элемент списка в нужное место на странице
   taskList.append(newTask);
-
-  //Очищаем поле ввода после добавления задачи
   taskInput.value = " ";
 });
-function()
 
-// Создаем функцию checkTask()
 function checkTask() {
-  const button = document.querySelector(".button");
-
-  //Внутри функции: Проверяем, что кликнули на элемент списка (event.target)
-  button.addEventListener("click", function (evt) {
-    if (evt.target.tagName === "BUTTON") {
-      // Если условие выполняется, то переключаем состояние выполнения задачи, добавляя или удаляя класс у элемента списка (element.classList.toggle)
-      evt.target.classList.toggle("aline-through");
+  taskList.addEventListener("click", function (evt) {
+    if (evt.target.tagName === "LI") {
+      evt.target.classList.toggle("completed");
     }
   });
 }
-
-// Добавляем функцию-обработчик к событию клика на кнопку (addEventListener)
-addButton.addEventListener("click", function() {});
-// Добавляем функцию-обработчик к событию клика на список задач (addEventListener)
-taskList.addEventListener("click", function() {});
+checkTask();
